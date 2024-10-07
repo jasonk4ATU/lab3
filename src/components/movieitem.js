6f321a4d0db7002f6c47bf84cd391c5901c9cc47
jasonk4ATU <g00394784@atu.ie>
2024-10-07 14:34:40 +0000

    UseEffect Update

--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -1,19 +1,25 @@
-import React from 'react';  // Import React library
+import React, { useEffect } from 'react';  // Import React and useEffect hook
 import { Card, Button } from 'react-bootstrap';  // Import Card and Button components from React Bootstrap
 
 const MovieItem = ({ movie }) => {  // Define the MovieItem component that accepts a 'movie' object as a prop
+
+    // useEffect to log movie details whenever the movie prop changes
+    useEffect(() => {
+        console.log("Movie Item:", movie);
+    }, [movie]);  // Only run this effect when the movie prop changes
+
     return (
-        <Card style={{ width: '18rem', marginBottom: '20px' }}>   {/*bootstrap styling for movie and poster*/}
+        <Card style={{ width: '18rem', marginBottom: '20px' }}>   {/* Bootstrap styling for movie and poster */}
             <Card.Img 
                 variant="top" 
                 src={movie.Poster}  // Display movie poster
             />
             <Card.Body>
                 <Card.Title> 
-                    {movie.Title} ({movie.Year})  {/*show movie title and year*/}
+                    {movie.Title} ({movie.Year})  {/* Show movie title and year */}
                 </Card.Title>
                 <Card.Text>
-                    Type: {movie.Type}  {/*show type (movie)*/}
+                    Type: {movie.Type}  {/* Show type (movie) */}
                 </Card.Text>
                 <Button 
                     href={`https://www.imdb.com/title/${movie.imdbID}/`}  // Link to the IMDB page for the movie
